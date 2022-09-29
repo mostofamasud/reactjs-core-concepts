4ce235cd03b20c98458933bf5b11ad28e5c0264e
mostofamasud <mostofamasud8@gmail.com>
2022-09-29 10:49:28 +0600

    Count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   
   return (
     <div className="App">
-    
+    <Counter></Counter>
     </div>
   );
 }
+
+function Counter (){
+  /* const increaseValue = () =>{
+    const newCount = count + 1;
+    setCount(newCount)
+  } */
+  const [count, setCount] = useState(44);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  return(
+    <div>
+      <h1>Count Number: {count}</h1>
+      <h1><button onClick={increaseCount}>Increase</button></h1>
+      <h1><button onClick={decreaseCount}>Decrease</button></h1>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
